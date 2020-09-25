4c9c05d37351a74d6bacb637a49b7d267976ad93
LucasLeibs <dr.lucasleibs@gmail.com>
2020-09-25 10:14:23 -0700

    set up controllers and CORS

--- oasis-backend/app/controllers/api/favorites_controller.rb
+++ oasis-backend/app/controllers/api/favorites_controller.rb
@@ -1,2 +1,35 @@
 class Api::V1::FavoritesController < ApplicationController
+    def index
+        favorites = Favorite.all 
+        render json: favorites
+    end
+
+    def create 
+        favorite = Favorite.new(favorite_params)
+        render json: favorite
+    end
+
+    def show 
+        favorite = Favorite.find(params[:id])
+        render json: favorite
+    end
+
+    def update 
+        favorite = Favorite.find(params[:id])
+        favorite.update(favorite_params)
+        render json: favorite
+    end
+
+    def destroy 
+        favorite = Favorite.find(params[:id])
+        favorite.destroy
+        render json:{message: "favorite has been deleted"}
+    end
+    private 
+
+    def favorite_params 
+        params.require(:favorite).permit(:rooftop_park_id, :user_id, :notes)
+    end
+
+
 end
--- oasis-backend/app/controllers/api/users_controller.rb
+++ oasis-backend/app/controllers/api/users_controller.rb
@@ -4,4 +4,31 @@ class Api::V1::UsersController < ApplicationController
         users = User.all 
         render json: users
     end
+
+    def create 
+        user = User.new(user_params)
+        render json: user
+    end
+
+    def show 
+        user = User.find(params[:id])
+        render json: user
+    end
+
+    def update 
+        user = User.find(params[:id])
+        user.update(user_params)
+        render json: user
+    end
+
+    def destroy 
+        user = User.find(params[:id])
+        user.destroy
+        render json:{message: "User has been deleted"}
+    end
+    private 
+
+    def user_params 
+        params.require(:user).permit(:name, :city_id)
+    end
 end
