0d809effa4e7f3ca4eb5c424fe5a3528cc6cd143
Taci Shlosberg <tshlosberg@gmail.com>
2020-09-30 10:43:22 -0700

    Removed home path

--- oasis-frontend/src/components/NavBar.js
+++ oasis-frontend/src/components/NavBar.js
@@ -14,7 +14,6 @@ class NavBar extends Component {
         return (
             <div className="navbar">
                 <NavLink to="/"> <img src= {logo} alt= 'oasis logo' className='logo'></img> </NavLink>
-                <NavLink to="/"> Home </NavLink>
                 <NavLink to="/rooftop_parks"> Rooftop Parks </NavLink>
                 {sessionStorage.getItem("Login") ? <a href="#" onClick={this.logout} > Logout </a> : <NavLink to="/login"> Login </NavLink> }
                 <NavLink to='/signup'> Sign Up </NavLink>
