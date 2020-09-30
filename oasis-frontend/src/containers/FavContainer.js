326a968f41efcfc275cbb29573e409ee7e97c62d
dbpatnodee <dbpatnode@hotmail.com>
2020-09-30 10:10:41 -0700

    cards added to favorites rendered properly.

--- oasis-frontend/src/containers/FavContainer.js
+++ oasis-frontend/src/containers/FavContainer.js
@@ -1,22 +1,62 @@
 import React, { Component } from 'react';
 import FrontCard from '../components/FrontCard'
+import BackCard from '../components/BackCard';
+import Flippy, { FrontSide, BackSide } from 'react-flippy';
 
 
 
-export default class FavContainer extends Component{
-    renderFavorites = () => {
-        return this.props.favorites.map(favorite => {
-           return <FrontCard park={favorite} key={favorite.id} addFavorite={this.props.addFavorite}  />
-       })
-       }
+class FavContainer extends Component{
+    // renderFavorites = () => {
+    //     return this.props.favorites.map(favorite => {
+    //        return <FrontCard park={favorite} key={favorite.id} addFavorite={this.props.addFavorite}  />
+    //    })
+    //    }
 
     
 
     render(){
+        // return (
+        //  <div>
+        //     {this.renderFavorites()}
+        // </div>
+        // )
+
         return (
-         <div>
-            {this.renderFavorites()}
-        </div>
-        )
+            <div>
+                <div>
+                    {this.props.favorites.map(favorite => {
+                        return (
+                            <div className="ml-5 mb-3" park={favorite} key={favorite.id} >
+                                <Flippy 
+                                    flipOnClick={true}
+                                    style={{ width: '300px' }}
+                                >
+                                <FrontSide
+                                    style={{ backgroundColor: '#ffffff' }}>
+                                    <FrontCard 
+                                        park={favorite} key={favorite.id} 
+                                        addFavorite={this.props.addFavorite}
+                                        onSelectFavorite={this.props.onSelectFavorite}
+                                        
+                                    />
+                                </FrontSide>
+                                <BackSide
+                                    style={{ backgroundColor: '#ffffff' }}>
+                                    <BackCard 
+                                        park={favorite} 
+                                        // onReviewClick={this.props.onReviewClick}
+                                    />
+                                </BackSide>
+                                </Flippy>
+                               
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
+        );
     }
 }
+
+
+export default FavContainer;
